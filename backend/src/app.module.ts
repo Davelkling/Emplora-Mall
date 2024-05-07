216fd35bd3865f830c94cb30d087d42be215d8e9
Jhon Dave Bohol <86525008+Davelkling@users.noreply.github.com>
2024-05-08 01:44:58 +0800

    resource init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,10 +7,15 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SupabaseModule } from './microservices/supabase/supabase.module';
+import { ProductsModule } from './products/products.module';
+import { StoresModule } from './stores/stores.module';
+import { VendorsModule } from './vendors/vendors.module';
+import { VendorProductsModule } from './vendor-products/vendor-products.module';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService], 
-  imports: [UsersModule, AuthModule,PrismaModule, CategoryModule, SupabaseModule],
+  imports: [UsersModule, AuthModule,PrismaModule, CategoryModule, SupabaseModule, ProductsModule, StoresModule, VendorsModule, VendorProductsModule, BrandsModule],
 })
 export class AppModule {}
